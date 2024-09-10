d69bf56a79f3bf51e273ceb0e9be34b7755d828a
Sudospective <avery@sudospective.net>
2024-09-10 15:22:42 -0700

    guh

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,32 @@
+#include <fstream>
 #include <iostream>
 #include <string>
 
+#include "TCPClient.h"
+
 int main(){
   std::string folder = "Temp";
+
+  std::ifstream receive("receive.txt");
+  std::ofstream send("send.txt");
+
+  std::function logger = [](const std::string& msg) {
+    std::cout << msg << std::endl;
+  };
+
+  CTCPClient tcp(logger);
+  if (tcp.Connect("127.0.0.1", "8765")) {
+    std::string out = (
+      std::string(1, static_cast<char>(2)) +
+      std::string(1, static_cast<char>(128)) +
+      "White Elephant Client"
+    );
+    std::string header = (
+      std::string(3, '\0') +
+      std::string(1, static_cast<char>(out.size()))
+    );
+    tcp.Send(header + out);
+  }
 
   return 0;
 }
