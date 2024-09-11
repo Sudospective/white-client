3e71d17d7867669e7c69f384bb3d7882e9d2e96d
Sudospective <avery@sudospective.net>
2024-09-11 03:17:43 -0700

    ITS ALIVE

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <thread>
 
+#include <nlohmann/json.hpp>
+
 #include "TCPClient.h"
 
 struct Gift {
@@ -18,7 +20,7 @@ int main() {
   std::vector<std::string> inputVec;
 
   std::function logger = [](const std::string& msg) {
-    std::cout << msg << std::endl;
+    //std::cout << msg << std::endl;
   };
   CTCPClient tcp(logger);
 
@@ -26,19 +28,19 @@ int main() {
 
   if (tcp.Connect("127.0.0.1", "8765")) {
     std::cout << "Connected." << std::endl;
-    std::string out = (
-      std::string(1, static_cast<char>(2)) +
-      std::string(1, static_cast<char>(128)) +
-      "White Elephant Client"
-    );
-    std::string header = (
-      std::string(3, '\0') +
-      std::string(1, static_cast<char>(out.size()))
-    );
-    tcp.Send(header + out);
+    std::ofstream status("status.txt", std::ios::trunc);
+    status << "connected" << std::endl;
+    status.close();
+    nlohmann::json j;
+    j.emplace("command", 2);
+    j.emplace("message", "Hello");
+    tcp.Send(j.dump());
   }
   else {
     std::cout << "Unable to connect to server." << std::endl;
+    std::ofstream status("status.txt", std::ios::trunc);
+    status << "inactive" << std::endl;
+    status.close();
   }
 
   std::function reader = [&]() {
@@ -46,11 +48,13 @@ int main() {
       char input[1024] = {};
       int read = tcp.Receive(input, 1024, false);
       if (read < 0) {
-        std::cout << "Read Error: " << read << std::endl;
         continue;
       }
       if (read == 0) {
         std::cout << "Connection lost." << std::endl;
+        std::ofstream status("status.txt", std::ios::trunc);
+        status << "inactive" << std::endl;
+        status.close();
         tcp.Disconnect();
       }
       mutex.lock();
@@ -73,28 +77,13 @@ int main() {
     inputVec.clear();
     mutex.unlock();
     for (std::string input : inputs) {
-      int proto = static_cast<int>(input[4]) + 128;
-      std::cout << "Server sent message on protocol " << proto << std::endl;
-      switch (proto) {
-        case 0: { // Ping
-          break;
-        }
-        case 1: { // Ping Response
-          break;
-        }
-        case 2: { // Hello
-          std::ofstream output("receive.txt");
-          output << "2" << input.erase(0, 6).erase(input.find_first_of('\0')) << std::endl;
-          output.close();
-          break;
-        }
-        case 3: {
-          break;
-        }
-        case 9: { // we can finally use it boys
-          break;
-        }
-      }
+      nlohmann::json j = nlohmann::json::parse(input);
+      int cmd = j["command"];
+      std::cout << "Server sent command " << cmd << std::endl;
+      std::ofstream output("receive.txt");
+      output << j.dump() << std::endl;
+      output.close();
+      break;
     }
 
     std::fstream send("send.txt", std::ios::in);
@@ -103,30 +92,18 @@ int main() {
     send.close();
 
     if (!data.empty()) {
-      int proto = std::stoi(std::string(1, data[0]));
-      if (!proto)
-        continue;
-      switch (proto) {
-        case 9: {
-          std::string out = (
-            std::string(1, static_cast<char>(proto)) +
-            data
-          );
-          std::string header = (
-            std::string(3, '\0') +
-            std::string(1, static_cast<char>(out.size()))
-          );
-          tcp.Send(header + out);
-          break;
-        }
-      }
+      // make sure its actually json
+      nlohmann::json j = nlohmann::json::parse(data);
+      tcp.Send(j.dump());
       // gross hack to clear the file
       send.open("send.txt", std::ios::out | std::ios::trunc);
       send.close();
     }
-
   }
 
+  std::ofstream status("status.txt", std::ios::trunc);
+  status << "inactive" << std::endl;
+  status.close();
 
   return 0;
 }
