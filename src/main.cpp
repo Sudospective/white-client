dc283d25c24ea1f29f1961ee3fe6cc1dfb10bf2c
Sudospective <avery@sudospective.net>
2024-09-13 10:51:38 -0700

    ping

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <ctime>
+
 #include <fstream>
 #include <iostream>
 #include <mutex>
@@ -9,8 +11,13 @@
 #include "TCPClient.h"
 
 int main() {
+  std::cout << "Type in server to connect to: ";
+  std::string serverAddress;
+  std::cin >> serverAddress;
+
   std::mutex mutex;
 
+  int serverOffset = 128;
   bool connected;
 
   std::vector<std::string> inputVec;
@@ -20,12 +27,13 @@ int main() {
   };
   CTCPClient tcp(logger);
 
+  // gross hack to clear the file
   std::ofstream fileCleaner("receive.txt", std::ios::trunc);
   fileCleaner.close();
 
   std::cout << "Connecting..." << std::endl;
 
-  if (tcp.Connect("127.0.0.1", "8765")) {
+  if (tcp.Connect(serverAddress, "9876")) {
     connected = true;
     std::cout << "Connected." << std::endl;
   }
@@ -59,24 +67,25 @@ int main() {
 
   std::thread thread = std::thread(reader);
 
-  while (true) {
+  while (connected) {
     mutex.lock();
     std::vector<std::string> inputs = inputVec;
     inputVec.clear();
     mutex.unlock();
     for (std::string input : inputs) {
-      nlohmann::json j;
-      try {
-        j = nlohmann::json::parse(input.erase(input.find_first_of('\0')));
-      }
-      catch (...) {
-        std::cout << "Error: invalid or malformed JSON." << std::endl;
-        continue;
-      }
+      nlohmann::json j = nlohmann::json::parse(input.erase(input.find_first_of('\0')));
       if (j == nullptr)
         continue;
       int cmd = j["command"];
       std::cout << "Server sent command " << cmd << std::endl;
+      if (cmd == serverOffset + 1) {
+        auto now = std::chrono::system_clock::now();
+        auto duration = now.time_since_epoch();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        int64_t time = ms - static_cast<int64_t>(j["data"]["timestamp"]);
+        std::cout << "Ping took " << time << "ms" << std::endl;
+        j["data"]["delay"] = time;
+      }
       std::ofstream output("receive.txt");
       output << j.dump() << std::endl;
       output.close();
@@ -95,10 +104,20 @@ int main() {
       }
       catch (...) {
         std::cout << "Error: invalid or malformed JSON." << std::endl;
-        // gross hack to clear the file
+        j["command"] = -1;
+        j["data"]["message"] = "Invalid JSON";
+        std::ofstream output("receive.txt");
+        output << j.dump() << std::endl;
+        output.close();
         send.open("send.txt", std::ios::out | std::ios::trunc);
         send.close();
         continue;
+      }
+      if (j["command"] == 0) {
+        auto now = std::chrono::system_clock::now();
+        auto duration = now.time_since_epoch();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(duration).count();
+        j["data"]["timestamp"] = ms;
       }
       tcp.Send(j.dump());
       send.open("send.txt", std::ios::out | std::ios::trunc);
